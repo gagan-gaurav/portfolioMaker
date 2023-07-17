4fafd59c35d2c70c4b656adc2d73b3a00fa62a31
Gagan Gaurav <iamgagangaurav@gmail.com>
2023-07-17 21:57:47 +0530

    Authenticator service added to verify JWT.

---
+++ src/service/app.authenticator.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+import { User } from './app.user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Authenticator {
+  constructor(private http: HttpClient, private cookieService: CookieService) {
+  }
+
+  public async authenticate(): Promise<boolean>{
+	if(this.cookieService.check('boonJwtToken')){
+		const jwtToken = this.cookieService.get('boonJwtToken');
+		const headers = new HttpHeaders({
+		  'Authorization': `Bearer ${jwtToken}` // Include the JWT token in the Authorization header
+		});
+		try{
+			const response = await this.http.get<any>('http://localhost:8080/api/v1/secured/verify_user', {headers: headers}).toPromise();
+			User.setUser(response.username);
+			return true;
+		} catch (error) {
+			console.log("JWT authentication error :", error);	
+		}
+	}
+	return false;
+  }
+}
