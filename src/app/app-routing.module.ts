e793719276d7df9f598ad6b56f12868262b16c02
Gagan Gaurav <iamgagangaurav@gmail.com>
2023-07-22 16:52:35 +0530

    some minor changes + welcome component added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,10 @@ import { LoginComponent } from 'src/lib/auth/login/login.component';
 import { SignupComponent } from 'src/lib/auth/signup/signup.component';
 import { FeedComponent } from 'src/lib/quilandquest/feed/feed.component';
 import { ContentComponent } from 'src/lib/quilandquest/content/content.component';
+import { WelcomeComponent } from 'src/lib/welcome/welcome.component';
 
 const routes: Routes = [
+  { path: "", component: WelcomeComponent},
   { path: "user/:username", component: WallComponent},
   { path: "login", component: LoginComponent},
   { path: "login/:source", component: LoginComponent},
