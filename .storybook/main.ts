8ab907dc3f2deebc6704795db7d9285383c14515
Gagan Gaurav <iamgagangaurav@gmail.com>
2023-07-31 03:21:59 +0530

    storybook build environment changed to dev

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,5 +13,16 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  webpackFinal: async (config: any) => {
+    //path to your Angular environment files
+    const envPath = require('path').join(__dirname, '../src/environments');
+
+    const envFile = 'environment';
+
+    // Updated the Webpack configuration to load the environment file
+    config.resolve.alias['environments'] = envPath + `/${envFile}.ts`;
+
+    return config;
+  },
 };
 export default config;
